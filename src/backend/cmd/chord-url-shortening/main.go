0ad91f6eac4e116e1c3219eb441069b49417db3f
Abram Tan <72917689+abramtan@users.noreply.github.com>
2024-10-30 21:02:40 +0800

    feat: implement gRPC, gin gonic, basic routes
    - Usage of env variables
    - Implement gRPC server
    - Implement http server using Gin Gonic
    - Basic health checks and routes for testing
    
    TODO:
    - Refactor and move code away from main.go

--- src/backend/cmd/chord-url-shortening/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-const NUMBER_OF_NODES = 5
-
-func main() {
-	// Initialize chord ring
-}
---
+++ src/backend/internal/utils/utils.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"os"
+	"strconv"
+)
+
+func GetEnvInt(key string, fallback int) (envInt int) {
+	if value, exists := os.LookupEnv(key); exists {
+		intValue, err := strconv.Atoi(value)
+		if err == nil {
+			envInt = intValue
+			return
+		}
+	}
+	envInt = fallback
+	return
+}
+
+func GetEnvFloat(key string, fallback float32) float32 {
+	if value, exists := os.LookupEnv(key); exists {
+		floatValue, err := strconv.ParseFloat(value, 32)
+		if err == nil {
+			return float32(floatValue)
+		}
+	}
+	return fallback
+}
+
+func GetEnvString(key string, fallback string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return fallback
+}
